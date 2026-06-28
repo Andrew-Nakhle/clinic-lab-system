43628e8f3eeb8f723ed63091d4b87aa8f08b8427
Andrew-Nakhle <andrewnakhle9@gmail.com>
2026-06-29 01:09:28 +0300

    Remove Laravel welcome page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
+//
+//use Illuminate\Support\Facades\Route;
+//
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => true,
+        'message' => 'Clinic Lab API is running successfully'
+    ]);
 });
