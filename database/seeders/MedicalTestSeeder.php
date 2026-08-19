4c49b83917a644c791574c343e7223d1bf5cf242
Andrew-Nakhle <andrewnakhle9@gmail.com>
2026-08-19 16:03:24 +0300

    edit medical test seeder

--- database/seeders/MedicalTestSeeder.php
+++ database/seeders/MedicalTestSeeder.php
@@ -6,66 +6,72 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\MedicalTest;
 
-class   MedicalTestSeeder extends Seeder
+class MedicalTestSeeder extends Seeder
 {
     public function run(): void
     {
         $tests = [
             [
-                'name' => 'Complete Blood Count',
-                'code' => 'CBC',
-                'price' => 15.00,
+                'name'         => 'CBC',
+                'code'         => 'CBC',
+                'price'        => 12.00,
                 'display_name' => 'صورة دم كاملة',
                 'normal_range' => '4.5 - 11.0 x10^3/µL'
             ],
             [
-                'name' => 'Fasting Blood Sugar',
-                'code' => 'FBS',
-                'price' => 10.00,
+                'name'         => 'Blood Sugar',
+                'code'         => 'FBS',
+                'price'        => 12.00,
                 'display_name' => 'سكر صائم',
                 'normal_range' => '70 - 100 mg/dL'
             ],
             [
-                'name' => 'Kidney Function Test',
-                'code' => 'KFT',
-                'price' => 25.00,
-                'display_name' => 'وظائف كلى (Creatinine)',
-                'normal_range' => '0.6 - 1.2 mg/dL'
+                'name'         => 'Vitamin D',
+                'code'         => 'VITD',
+                'price'        => 12.00,
+                'display_name' => 'فحص فيتامين د',
+                'normal_range' => '30 - 100 ng/mL'
             ],
             [
-                'name' => 'Liver Function Test',
-                'code' => 'LFT',
-                'price' => 30.00,
-                'display_name' => 'وظائف كبد (ALT)',
-                'normal_range' => '7 - 56 U/L'
-            ],
-            [
-                'name' => 'Lipid Profile',
-                'code' => 'LIPID',
-                'price' => 20.00,
-                'display_name' => 'الكوليسترول والدهون الثلاثية',
+                'name'         => 'Cholesterol',
+                'code'         => 'LIPID',
+                'price'        => 12.00,
+                'display_name' => 'الكوليسترول والدهون',
                 'normal_range' => 'Desirable < 200 mg/dL'
             ],
             [
-                'name' => 'Thyroid Stimulating Hormone',
-                'code' => 'TSH',
-                'price' => 35.00,
+                'name'         => 'Iron Test',
+                'code'         => 'FE',
+                'price'        => 12.00,
+                'display_name' => 'فحص الحديد',
+                'normal_range' => '60 - 170 mcg/dL'
+            ],
+            [
+                'name'         => 'Thyroid Test',
+                'code'         => 'TSH',
+                'price'        => 12.00,
                 'display_name' => 'هرمون الغدة الدرقية',
                 'normal_range' => '0.4 - 4.0 mIU/L'
             ],
             [
-                'name' => 'Vitamin D Test',
-                'code' => 'VITD',
-                'price' => 50.00,
-                'display_name' => 'فحص فيتامين د',
-                'normal_range' => '30 - 100 ng/mL'
+                'name'         => 'Kidney Function',
+                'code'         => 'KFT',
+                'price'        => 12.00,
+                'display_name' => 'وظائف كلى',
+                'normal_range' => '0.6 - 1.2 mg/dL'
+            ],
+            [
+                'name'         => 'Liver Function',
+                'code'         => 'LFT',
+                'price'        => 12.00,
+                'display_name' => 'وظائف كبد',
+                'normal_range' => '7 - 56 U/L'
             ],
         ];
 
-        // التعديل السحري لحل مشكلة الـ Duplicate Entry
         foreach ($tests as $test) {
             MedicalTest::updateOrCreate(
-                ['code' => $test['code']], // يبحث أولاً بكود التحليل لمنع التكرار
+                ['code' => $test['code']],
                 [
                     'name'         => $test['name'],
                     'price'        => $test['price'],
