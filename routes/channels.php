bc9e497f23e957666ee4e86ffcbd9c371306dbd2
Andrew-Nakhle <andrewnakhle9@gmail.com>
2026-08-22 04:07:51 +0300

    fixes

--- routes/channels.php
+++ routes/channels.php
@@ -5,12 +5,13 @@ use App\Models\User;
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-
-Broadcast::channel('chat.{receiverId}', function ($user, $receiverId) {
+Broadcast::channel('chat.{userId}', function ($user, $userId) {
+    return (int) $user->id === (int) $userId;
     // الشرط: نسمح للمستخدم بالاشتراك في القناة فقط إذا كان هو صاحب الرسالة
     // أو إذا كان هو الشخص المستقبِل لها.
-    return (int) $user->id === (int) $receiverId;
 });
+
+
 Broadcast::channel('notifications.{userId}', function ($user, $userId) {
     return (int) $user->id === (int) $userId;
 });
